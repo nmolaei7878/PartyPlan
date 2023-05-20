5f350164677b778515ebda79895b58ce684e699a
mohammad <mohammad@mohammads-MacBook-Pro.local>
2023-05-21 02:08:30 +0330

    add to page header

--- src/pages/todo/Todo.tsx
+++ src/pages/todo/Todo.tsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useCallback, useEffect } from "react";
+import BackIcon from "../../assets/icons/back_icon.svg";
+import { useLocation, useNavigate } from "react-router-dom";
+import { useAppSelector } from "../../core/hook/hooks";
 
 const Todo = () => {
-  return <div>Todo</div>;
+  const { state } = useLocation();
+  const { id } = state;
+  const navigate = useNavigate();
+  const plans = useAppSelector((state) => state.plan);
+
+  useEffect(() => {
+    const foundedPlan = plans.plans.find((e) => e.id === id);
+  }, []);
+
+  const goBack = useCallback(() => {
+    navigate(-1);
+  }, []);
+  return (
+    <div className="h-full w-full text-white text-lg font-bold flex flex-col justify-between">
+      <div className="h-1/20 ">
+        <div className="flex items-center gap-3  ">
+          <img onClick={goBack} className="w-8" src={BackIcon} alt="" />
+          <p className="text-xl">Checklist</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Todo;
