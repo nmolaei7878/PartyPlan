0f69e7a3588f720d42fb98d22399090b196eaf8d
mohammad <mohammad@mohammads-MacBook-Pro.local>
2023-05-26 19:14:54 +0330

    plan slice test updated

--- src/redux/__test__/plan-slice.test.ts
+++ src/redux/__test__/plan-slice.test.ts
@@ -1,5 +1,15 @@
 import store from "../store";
-import { createPlan, updateOccasion, addPlan } from "../slices/plan-slice";
+import {
+  createPlan,
+  updateOccasion,
+  updateFood,
+  updateAlcohol,
+  updateDecorator,
+  updateGuest,
+  resetPlan,
+  updateEventName,
+  addPlan,
+} from "../slices/plan-slice";
 
 test("create Plan test", () => {
   let state = store.getState().plan;
@@ -9,12 +19,34 @@ test("create Plan test", () => {
   expect(state.newPlan?.id).toEqual("1");
 });
 
-test("update Occasion test", () => {
+test("update Plan Object test", () => {
   let state = store.getState().plan.newPlan;
   expect(state?.id).toEqual("1");
   store.dispatch(updateOccasion("other"));
+  store.dispatch(updateAlcohol("no"));
+  store.dispatch(updateDecorator("yes"));
+  store.dispatch(updateEventName("BirthDay"));
+  store.dispatch(updateFood("Potluck"));
+  store.dispatch(updateGuest("large"));
   state = store.getState().plan.newPlan;
   expect(state?.occasion).toEqual("other");
+  expect(state).toMatchInlineSnapshot(`
+    Object {
+      "alcohol": "no",
+      "boardGame": "",
+      "budget": "",
+      "decorator": "yes",
+      "eInvite": "",
+      "eventDate": "",
+      "eventName": "BirthDay",
+      "eventTime": "",
+      "foodArrangment": "Potluck",
+      "guestSize": "large",
+      "id": "1",
+      "occasion": "other",
+      "todos": Array [],
+    }
+  `);
 });
 
 test("add Plan test", () => {
@@ -24,4 +56,11 @@ test("add Plan test", () => {
   store.dispatch(addPlan());
   state = store.getState().plan;
   expect(state?.plans).toHaveLength(1);
+});
+
+test("reset Plan test", () => {
+  let state = store.getState().plan.newPlan;
+  store.dispatch(resetPlan());
+  state = store.getState().plan.newPlan;
+  expect(state).toEqual(undefined);
 });
