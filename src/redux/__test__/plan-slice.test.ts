dff886828b6720e7818d8e2dcdd868518ec6c6fd
mohammad <mohammad@mohammads-MacBook-Pro.local>
2023-05-26 18:40:22 +0330

    plan-slice test

---
+++ src/redux/__test__/plan-slice.test.ts
@@ -0,0 +1,27 @@
+import store from "../store";
+import { createPlan, updateOccasion, addPlan } from "../plan-slice";
+
+test("create Plan test", () => {
+  let state = store.getState().plan;
+  expect(state.newPlan).toEqual(undefined);
+  store.dispatch(createPlan("1"));
+  state = store.getState().plan;
+  expect(state.newPlan?.id).toEqual("1");
+});
+
+test("update Occasion test", () => {
+  let state = store.getState().plan.newPlan;
+  expect(state?.id).toEqual("1");
+  store.dispatch(updateOccasion("other"));
+  state = store.getState().plan.newPlan;
+  expect(state?.occasion).toEqual("other");
+});
+
+test("add Plan test", () => {
+  let state = store.getState().plan;
+  expect(state?.plans).toEqual([]);
+  expect(state?.plans).toHaveLength(0);
+  store.dispatch(addPlan());
+  state = store.getState().plan;
+  expect(state?.plans).toHaveLength(1);
+});
